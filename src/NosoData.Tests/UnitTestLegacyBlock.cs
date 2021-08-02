8e25fa35a0d0559d173f165cd62daea9bf6f99f1
Gustavo Carreno <guscarreno@gmail.com>
2021-08-02 21:41:12 +0100

    feat: Started loading from file and test

--- src/NosoData.Tests/UnitTestLegacyBlock.cs
+++ src/NosoData.Tests/UnitTestLegacyBlock.cs
@@ -6,13 +6,54 @@ namespace Noso.Data.Tests
 {
     public class UnitTestLegacyBlock
     {
+        private void CheckFieldsCreate(LegacyBlock block)
+        {
+            Assert.Equal(-1, block.Number);
+            //Assert.Equal("", block.Hash);
+            Assert.Equal(-1, block.TimeStart);
+            Assert.Equal(-1, block.TimeEnd);
+            Assert.Equal(-1, block.TimeTotal);
+            Assert.Equal(-1, block.TimeLast20);
+            Assert.Equal(-1, block.Difficulty);
+            Assert.Equal("", block.TargetHash);
+            Assert.Equal("", block.Solution);
+            Assert.Equal("", block.LastBlockHash);
+            Assert.Equal(-1, block.NextBlockDifficulty);
+            Assert.Equal("", block.Miner);
+            Assert.Equal(0, block.Fee);
+            Assert.Equal(0, block.Reward);
+        }
+
+        private void CheckFieldsBlockZero(LegacyBlock block)
+        {
+            Assert.Equal(-1, block.Number);
+            //Assert.Equal("", block.Hash);
+            Assert.Equal(1531896783, block.TimeStart);
+            Assert.Equal(1615132800, block.TimeEnd);
+            Assert.Equal(83236017, block.TimeTotal);
+            Assert.Equal(600, block.TimeLast20);
+            Assert.Equal(60, block.Difficulty);
+            Assert.Equal("", block.TargetHash);
+            Assert.Equal("", block.Solution);
+            Assert.Equal("NOSO GENESYS BLOCK", block.LastBlockHash);
+            Assert.Equal(60, block.NextBlockDifficulty);
+            Assert.Equal("N4PeJyqj8diSXnfhxSQdLpo8ddXTaGd", block.Miner);
+            Assert.Equal(0, block.Fee);
+            Assert.Equal(1030390730000, block.Reward);
+        }
+        
         [Fact]
         public void TestCreate()
         {
             var block = new LegacyBlock();
-
-            Assert.Equal(-1, block.Number);
-            Assert.Equal("", block.Hash);
+            CheckFieldsCreate(block);
+        }
+        
+        [Fact]
+        public void TestBlockZero()
+        {
+            var block = new LegacyBlock(@"../test-data/NOSODATA/BLOCKS/0.blk");
+            CheckFieldsBlockZero(block);
         }
     }
 }
--- src/NosoData/LegacyBlock.cs
+++ src/NosoData/LegacyBlock.cs
@@ -1,13 +1,63 @@
-﻿namespace Noso.Data.Legacy
+﻿using System;
+using System.IO;
+
+namespace Noso.Data.Legacy
 {
     public class LegacyBlock
     {
-        public int Number { get; set; }
-        public string Hash { get; }
+        public long Number { get; set; }
+        //public string Hash { get; }
+        public long TimeStart { get; set; }
+        public long TimeEnd { get; set; }
+        public int TimeTotal { get; set; }
+        public int TimeLast20 { get; set; }
+        public int Difficulty { get; set; }
+        public string TargetHash { get; set; }
+        public string Solution { get; set; }
+        public string LastBlockHash { get; set; }
+        public int NextBlockDifficulty { get; set; }
+        public string Miner { get; set; }
+        public long Fee { get; set; }
+        public long Reward { get; set; }
 
-        public LegacyBlock() {
+        public LegacyBlock()
+        {
+            this.InitFields();
+        }
+
+        public LegacyBlock(string AFilename)
+        {
+            this.InitFields();
+            this.doLoadFromFile(AFilename);
+        }
+
+        private void InitFields()
+        {
             this.Number = -1;
-            this.Hash = "";
+            //this.Hash = "";
+            this.TimeStart = -1;
+            this.TimeEnd = -1;
+            this.TimeTotal = -1;
+            this.TimeLast20 = -1;
+            this.Difficulty = -1;
+            this.TargetHash = "";
+            this.Solution = "";
+            this.LastBlockHash ="";
+            this.NextBlockDifficulty = -1;
+            this.Miner = "";
+            this.Fee = 0;
+            this.Reward = 0;
+        }
+
+        private void doLoadFromFile(string AFilename)
+        {
+            if (File.Exists(AFilename))
+            {
+                using (BinaryReader brBlock = new BinaryReader(File.Open(AFilename, FileMode.Open)))
+                {
+                    this.Number = brBlock.ReadInt64();
+                }
+            }
         }
     }
 }
